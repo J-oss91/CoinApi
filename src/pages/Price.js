761b3fe831ed3800a3bc9592464a45b3e6260129
j-oss91 <joscelyn.s.davis@gmail.com>
2023-08-04 14:11:50 -0400

    second commit

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -9,13 +9,63 @@ export default function Price(props) {
    const apiKey = "82B3B31D-12B8-4F3F-9B30-4CDC68351E87";
 
    //Grab the Currency symbol from the URL Params
-    const params = useParams();
-    const symbol = params.symbol;
-   // Using the other two varibles to create URL
-   const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`
-  return (
-    <div>
-        <h1>This is the Price component</h1>
-    </div>
-  )
+    const params = useParams()
+    const symbol = params.symbol
+   
+   
+    // Using the other two varibles to create URL
+   const url = `http://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+  
+
+   //Set State to hold coin data
+   const[coin, setCoin] = useState("null")
+
+   //Creat a function to get coin data
+   const getCoin = async () => {
+     const response = await fetch(url);
+     const data = await response.json();
+     setCoin(data);
+   };
+
+  //  const getCoin =  async () => {
+  //   try{
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     setCoin(data);
+  //   } catch (e){
+  //     console.error(e)
+  //   }
+  //  };
+
+   //Use useEffect to run getCoin
+   useEffect(() => {
+    getCoin();
+   }, []);
+   
+   //Loaded function for when data is fetched
+   const loaded = () => {
+     return(
+      <div>
+        <h1>
+          {coin.asset_id_base}/{coin.asset_id_quote}
+        </h1>
+        <h2>
+          {coin.rate}
+        </h2>
+
+      </div>
+     );
+   };
+
+   // Function for data that does not exist
+   const loading = () => {
+    return(
+      <h1> Loading...</h1>
+    );
+   };
+
+   //If coin has data, run the loaded function, otherwise, run loaded function
+   return (
+    coin && coin.rate ? loaded() : loading()
+  );
 };
